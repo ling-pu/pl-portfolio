7ff8fd60683038a3a129d9be154865f8003de7b5
ling-pu <impu317@gmail.com>
2025-07-31 14:15:51 +0800

    delete data#1

--- pl-portfolio/src/data/images.js
+++ pl-portfolio/src/data/images.js
@@ -1,6 +1,5 @@
 // src/data/images.js
 export const images = [
-    { src: '/images/gallery1.jpg', alt: '台北捷運高中團契分布圖', year:'2025' },
     { src: '/images/gallery2.jpg', alt: '日本選物代購品牌官網', year:'2025' },
     { src: '/images/gallery3.jpg', alt: '咖啡電商品牌官網', year:'2025'},
     { src: '/images/gallery4.png', alt: 'Y2K Icons', year:'2025' },
